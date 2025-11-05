e4ebe6055c0b1be65d389c1c97299a3a9c9d18e2
Adam Thomas <thomasadam83@hotmail.com>
2025-11-05 09:22:14 -0500

    added sso code to start

---
+++ action/logout.php
@@ -0,0 +1,10 @@
+<?php
+
+setcookie('jwt', '', time() - 3600, '/', 'brickmmo.com', false, false);
+unset($_SESSION['user']);
+
+setcookie('hash_id', '', time() - 1, '/', 'brickmmo.com');
+setcookie('hash_string', '', time() - 1, '/', 'brickmmo.com');
+
+message_set('Logged Out Success', 'You have successfully been logged out!');
+header_redirect('/login');
