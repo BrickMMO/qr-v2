409099fb1746281ce1d990dfd24e1bc14a050946
Adam Thomas <thomasadam83@hotmail.com>
2025-11-05 19:57:26 -0500

    Fixed slideout

--- qr/list.php
+++ qr/list.php
@@ -1,7 +1,6 @@
 <?php
 
-define('APP_NAME', 'My Account');
-
+define('APP_NAME', 'QR Codes');
 define('PAGE_TITLE', 'Dashboard');
 define('PAGE_SELECTED_SECTION', '');
 define('PAGE_SELECTED_SUB_PAGE', '');
@@ -9,6 +8,7 @@ define('PAGE_SELECTED_SUB_PAGE', '');
 include('../templates/html_header.php');
 include('../templates/nav_header.php');
 include('../templates/nav_slideout.php');
+include('../templates/nav_sidebar.php');
 include('../templates/main_header.php');
 
 $query = 'SELECT qrs.*,(
--- templates/nav_sidebar.php
+++ templates/nav_sidebar.php
@@ -1,62 +1,66 @@
-<?php
+<?php if(PAGE_SELECTED_SUB_PAGE): ?>
 
-$navigation = navigation_array(PAGE_SELECTED_SUB_PAGE);
+    <?php
 
-?>
+    $navigation = navigation_array(PAGE_SELECTED_SUB_PAGE);
 
-<!-- SUB NAV -->
+    ?>
 
-<nav
-    class="w3-sidebar w3-bar-block w3-border-right"
-    id="sidebar-sub"
-    style="
-    width: 100%;
-    max-width: 240px;
-    z-index: 108;
-    top: 0px;
-    padding-top: 58px;
-    "
->
-    <div class="w3-padding-16 w3-border-bottom">
-        <div class="w3-bar-item w3-text-gray bm-caps">
-            <i class="<?=$navigation['icon']?> fa-padding-right"></i> 
-            <?php if(isset($navigation['title'])): ?>
-                <?=$navigation['title']?>
-            <?php endif; ?>
+    <!-- SUB NAV -->
+
+    <nav
+        class="w3-sidebar w3-bar-block w3-border-right"
+        id="sidebar-sub"
+        style="
+        width: 100%;
+        max-width: 240px;
+        z-index: 108;
+        top: 0px;
+        padding-top: 58px;
+        "
+    >
+        <div class="w3-padding-16 w3-border-bottom">
+            <div class="w3-bar-item w3-text-gray bm-caps">
+                <i class="<?=$navigation['icon']?> fa-padding-right"></i> 
+                <?php if(isset($navigation['title'])): ?>
+                    <?=$navigation['title']?>
+                <?php endif; ?>
+            </div>
         </div>
-    </div>
 
-    <div class="w3-padding-16 w3-border-bottom">
-    
-        <?php foreach($navigation['sub-pages'] as $page): ?>
-
-            <?php if(isset($page['title'])): ?>
-
-                <a
-                    class="w3-bar-item w3-button w3-text-<?=$page['colour']?> <?php if($page['url'] == PAGE_SELECTED_SUB_PAGE): ?>bm-selected<?php endif; ?>"
-                    href="<?=(strpos($page['url'], 'http') === 0) ? '' : ENV_DOMAIN?><?=$page['url']?>"
-                >
-                    <?php if(isset($page['icon'])): ?>
-                        <?php if(substr($page['icon'], 0, 2) == 'fa'): ?>
-                            <i class="<?=$page['icon']?> fa-padding-right"></i> 
-                        <?php elseif(substr($page['icon'], 0, 2) == 'bm'): ?>
-                            <i class="<?=$page['icon']?> fa-padding-right"></i> 
-                        <?php endif; ?>
-                    <?php endif; ?> 
-                    <?=$page['title']?>   
-                </a>
-
-            <?php else: ?>    
-
-                </div>
-                <div class="w3-padding-16 w3-border-bottom">
-
-            <?php endif; ?>
-
-        <?php endforeach; ?>
+        <div class="w3-padding-16 w3-border-bottom">
         
-    </div>
-</nav>
+            <?php foreach($navigation['sub-pages'] as $page): ?>
+
+                <?php if(isset($page['title'])): ?>
+
+                    <a
+                        class="w3-bar-item w3-button w3-text-<?=$page['colour']?> <?php if($page['url'] == PAGE_SELECTED_SUB_PAGE): ?>bm-selected<?php endif; ?>"
+                        href="<?=(strpos($page['url'], 'http') === 0) ? '' : ENV_DOMAIN?><?=$page['url']?>"
+                    >
+                        <?php if(isset($page['icon'])): ?>
+                            <?php if(substr($page['icon'], 0, 2) == 'fa'): ?>
+                                <i class="<?=$page['icon']?> fa-padding-right"></i> 
+                            <?php elseif(substr($page['icon'], 0, 2) == 'bm'): ?>
+                                <i class="<?=$page['icon']?> fa-padding-right"></i> 
+                            <?php endif; ?>
+                        <?php endif; ?> 
+                        <?=$page['title']?>   
+                    </a>
+
+                <?php else: ?>    
+
+                    </div>
+                    <div class="w3-padding-16 w3-border-bottom">
+
+                <?php endif; ?>
+
+            <?php endforeach; ?>
+            
+        </div>
+    </nav>
+
+<?php endif; ?>
 
 <script>
 
@@ -111,4 +115,3 @@ $navigation = navigation_array(PAGE_SELECTED_SUB_PAGE);
 </script>
 
 
-            
